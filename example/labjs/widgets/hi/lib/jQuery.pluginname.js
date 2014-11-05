780da8b54b85482ce290ef4cbae52d554b28e3b6
Robert Cooke <robert.cooke@trimm.nl>
2014-11-05 19:01:13 +0100

    cc

--- example/labjs/widgets/hi/lib/jQuery.pluginname.js
+++ example/labjs/widgets/hi/lib/jQuery.pluginname.js
@@ -1,37 +1,13 @@
-
-;(function($) {
-	/*
-	 * Public methods 
-	 */
-
+;
+(function($) {
 	$.fn.pluginname = function(options) {
 		if (!$(this).length) {
 			return this;
 		}
-
-		$(this)
-			.data('pluginname', $.extend({}, options, ($.metadata ? $(this).metadata() : {})))
-			.unbind('click.fb')
-			.bind('click.fb', function(e) {
-				e.preventDefault();
-				return;
-			});
-
+		this.defaultOptions = {};
+		var settings = $.extend({}, this.defaultOptions, options);
 		return this;
 	};
-
 	$.pluginname = function(obj) {
-
 	};
-
-	$.pluginname.init = function() {
-	};
-
-	$.fn.pluginname.defaults = {
-	};
-
-	$(document).ready(function() {
-		$.pluginname.init();
-	});
-
 })(jQuery);
